f826a2c132cc20d2dcb0cf9d3cdbd15ba2a8159c
MohammedHassanXO <mohammedhassan12_5@yahoo.com>
2024-11-27 20:01:05 +0200

    rendering from list

--- src/index.js
+++ src/index.js
@@ -58,14 +58,14 @@ function App() {
 };
 
 function Pizza(props) {
-    return <div className='pizza'>
-        <img src={props.photoName} alt={props.name} />
+    return <li className='pizza'>
+        <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
         <div>
-            <h3>{props.name}</h3>
-            <p>{props.ingredients}</p>
-            <span>{props.price}</span>
+            <h3>{props.pizzaObj.name}</h3>
+            <p>{props.pizzaObj.ingredients}</p>
+            <span>{props.pizzaObj.price}</span>
         </div>
-    </div>
+    </li>
 }
 
 function Header() {
@@ -77,8 +77,11 @@ function Header() {
 function Menu() {
     return <main className='menu'>
         <h2>Our Menu</h2>
-        <Pizza name="Pizza Prosciutto" photoName="pizzas/prosciutto.jpg" ingredients="Tomato, mozarella, ham, aragula, and burrata cheese" price={18} soldOut={false} />
-        <Pizza name="Pizza Prosciutto" photoName="pizzas/prosciutto.jpg" ingredients="Tomato, mozarella, ham, aragula, and burrata cheese" price={18} soldOut={false} />
+        <ul className='pizzas'>
+            {pizzaData.map(
+                (pizza) => <Pizza pizzaObj={pizza} key={pizza.name} />
+            )}
+        </ul>
     </main>
 }
 
