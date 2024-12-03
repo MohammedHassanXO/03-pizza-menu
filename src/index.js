5afd6b392328e42d8c080bdc5912b84205ae7d52
MohammedHassanXO <mohammedhassan12_5@yahoo.com>
2024-12-03 17:42:42 +0200

    edited footer

--- src/index.js
+++ src/index.js
@@ -89,10 +89,20 @@ function Footer() {
     const hour = new Date().getHours();
     const openHour = 8;
     const closeHour = 22;
-    const isOpen = hour >= openHour && hour <= closeHour ? "We're open now" : "We're closed now";
+    const isOpen = hour >= openHour && hour <= closeHour;
 
     return <footer className='footer'>
-        {isOpen}
+        {isOpen && (
+            <div className='order'>
+                <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+                <button className='btn'>order</button>
+            </div>
+        )}
+        {!isOpen && (
+            <div className='order'>
+                <p>We're closed. We'll open {openHour}:00.</p>
+            </div>
+        )}
     </footer>
 }
 
